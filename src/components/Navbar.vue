8c7e7b0e40dd65df979410b2a4af422e0a5060a3
Rafael Pianaro <rafaelpianaro@gmail.com>
2020-10-09 11:22:27 -0300

    added styles on Navbar, created services array

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -8,10 +8,53 @@
 
 <script>
 export default {
-    name: 'navbar'
+    name: 'navbar',
+    data(){
+        return {
+            services: [
+                {
+                    title: 'Web',
+                    link: '#'
+                },
+                {
+                    title: 'Design',
+                    link: '#'
+                },
+                {
+                    title: 'Videos',
+                    link: '#'
+                }
+            ]
+        }
+    }
 }
 </script>
 
 <style>
+nav {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
 
+nav .menu-item {
+    color: #fff;
+    padding: 10px 20px;
+    position: relative;
+    text-align: center;
+    border-bottom: 3px solid transparent;
+    display: flex;
+    transition: 0.4s;
+}
+
+nav .menu-item.active,
+nav .menu-item:hover {
+    background-color: #444;
+    border-bottom-color: #ff5858;
+}
+
+nav .menu-item a {
+    color: inherit;
+    text-decoration: none;
+}
 </style>
