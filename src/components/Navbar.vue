622555bf2e50d42b6f0d3b03dd76c581114577f1
Rafael Pianaro <rafaelpianaro@gmail.com>
2020-10-09 14:24:13 -0300

    added style for dropdown

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,13 +2,19 @@
   <nav>
       <div class="menu-item"><a href="#">Home</a></div>
       <div class="menu-item"><a href="#">About</a></div>
+      <Dropdown title="Services" :items="services" />
       <div class="menu-item"><a href="#">Contact</a></div>
   </nav>
 </template>
 
 <script>
+import Dropdown from './Dropdown'
+
 export default {
     name: 'navbar',
+    components: {
+        Dropdown
+    },
     data(){
         return {
             services: [
